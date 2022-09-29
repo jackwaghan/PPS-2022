adf89abee8f5a375712a065df8bd8134cc23bf58
JackWaghan <102263597+Jack-Waghan@users.noreply.github.com>
2022-09-29 18:58:34 +0530

    Update Increment.cpp

--- Increment.cpp
+++ Increment.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h>
-int main()\
+int main()
 {
- int date=7;
- int month=10;
- int year=2022;
- printf("Your start date is : %d/%d/%d", date,month, year);
- int date1 = 7+date;
- printf("\nIncrement By 7 Days : %d/%d/%d", date1,month, year);
- int month1 = 1+month;
- printf("\nIncrement By 1 month : %d/%d/%d", date1,month1,year);
- int month2 = month-9;
- int date3= date-6;
- int year1= 1+year;
- printf("\nuntil the end of the year : %d/%d/%d",date3,month2,year1 );
+ int dd,mm,yyyy;
+ printf("Enter Your date : ");
+ scanf("%d", &dd );
+ printf("Enter Your month : ");
+ scanf("%d",&mm);
+ printf("Enter Your year : ");
+ scanf("%d",&yyyy);
+ int dd1 = 7+dd;
+ printf("\nIncrement By 7 Days : %d/%d/%d", dd1,mm, yyyy);
+ int mm1 = 1+mm;
+ printf("\nIncrement By 1 month : %d/%d/%d", dd1,mm1,yyyy);
+ int mm2 = mm-9;
+ int dd3= dd-6;
+ int yyyy1= 1+yyyy;
+ printf("\nuntil the end of the year : %d/%d/%d",dd3,mm2,yyyy1 );
 
 }
